741ca2b2320190b011f3e34616ef28a01a8b5bc8
oRonold <or040918@gmail.com>
2024-08-13 13:13:29 -0300

    permitindo requisicoes com endpoint "/usuario"

--- relacoes/src/main/java/br/com/fiap/relacoes/security/SecurityConfigurations.java
+++ relacoes/src/main/java/br/com/fiap/relacoes/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.fiap.relacoes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,9 +12,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/usuario").permitAll();
+                    req.anyRequest().authenticated();
+                })
                 .build();
     }
 }
